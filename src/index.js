1f188e1550f6b5f32fb59bfe9d4f65fe04c54cdf
RoseGabriely <rosegabriely14@gmail.com>
2021-10-08 16:00:19 -0400

    created a store and connected reducer, thunk and logger

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Provider } from "react";
+import { createStore, applyMiddleware } from "redux";
+import reducer from "./reducers";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
 import "./index.css";
 import App from "./App";
 
-const { worker } = require('./mocks/browser');
+const { worker } = require("./mocks/browser");
 worker.start();
 
 const rootElement = document.getElementById("root");
 
+const store = createStore(reducer, applyMiddleware(logger, thunk));
+
 ReactDOM.render(
-    <App />, 
-    rootElement
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
 );
 
 //Task List:
